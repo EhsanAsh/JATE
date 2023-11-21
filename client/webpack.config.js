f6c4705222d213fd8b9d03deb0ceae98700848c7
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-11-21 18:13:59 -0500

    feat(Rules): Add workbox plugins, CSS loaders, and babel to webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,9 +11,8 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 // ===============================================================
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
+// Exporting webpack config
+// ===============================================================
 module.exports = () => {
     return {
         mode: 'development',
@@ -27,13 +26,16 @@ module.exports = () => {
         },
         plugins: [
 
+            // Plugin to add html file to dist folder
             new HtmlWebpackPlugin({
                 template: './index.html',
                 title: 'J.A.T.E'
             }),
 
+            // adding mini css extract plugin to extract css to a separate file
             new MiniCssExtractPlugin(),
 
+            // adding inject manifest plugin to inject manifest file into service worker
             new InjectManifest({
                 swSrc: './src-sw.js',
                 swDest: 'sw.js',
@@ -41,10 +43,36 @@ module.exports = () => {
 
         ],
 
+        // setting rules for webpack to follow when building project files
         module: {
             rules: [
+
+                {
+                    test: /\.css$/i,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader'],
+                },
+
+                {
+                    test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+                    use: 'asset/resource',
+                },
+
+                {
+                    test: /\.m?js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'], // this preset will include all plugins to support modern JavaScript
+                            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-transform-runtime'],
+                            // the first plugin is used to transpile object rest and spread properties to ES5
+                            // the second plugin is used to avoid duplication of helper functions in every file
+                        },
+                    },
+                },
 
             ],
         },
     };
 };
+// ===============================================================
